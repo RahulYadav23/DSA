47a388c3d9ffff8e8e004c772a8a83febf0e50bd
Rahul Yadav <RahulYadav23@users.noreply.github.com>
2022-08-16 06:27:02 +0530

    added heap and queue

--- Newton-School/src/com/graph/TopologicalSort.java
+++ Newton-School/src/com/graph/TopologicalSort.java
@@ -1,6 +1,8 @@
 package com.graph;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
 
 class TGraph {
     ArrayList<Integer>[] adj;
@@ -16,7 +18,52 @@ class TGraph {
     public void addEdges(int source, int destination) {
         adj[source].add(destination);
     }
+
+    public void topologicalSort()
+    {
+        boolean[] visited = new boolean[adj.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<adj.length;i++)
+        {
+            if(!visited[i])
+            {
+                topologicalsort(visited,st,i);
+            }
+        }
+
+        while (!st.empty())
+        {
+            System.out.print(st.pop()+" ");
+        }
+
+    }
+    private void topologicalsort(boolean[] visited,Stack<Integer>st,int source)
+    {
+        visited[source]=true;
+        for(int n:adj[source])
+        {
+            if(!visited[n])
+            {
+                topologicalsort(visited,st,n);
+            }
+        }
+        st.push(source);
+    }
 }
 public class TopologicalSort {
-
+    public static void main(String[] args)
+    {    Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of vertex");
+        int n  = sc.nextInt();
+        pathGraph graph = new pathGraph(n);
+        System.out.println("Enter the number of edges");
+        int e = sc.nextInt();
+        System.out.println("Enter the edges");
+        for(int i=0;i<n;i++)
+        {
+            int source= sc.nextInt();
+            int destination = sc.nextInt();
+            graph.addEdges(source,destination);
+        }
+    }
 }
